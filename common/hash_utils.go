45f7b92b8ec97ae59f693c987311344243132374
creamwhip <dev3@swingby.network>
2022-05-09 14:17:06 +0800

    KS-IOF-F-10 Security audit. Rejection Sampling Unimplemented

--- common/hash_utils.go
+++ common/hash_utils.go
@@ -13,6 +13,25 @@ import (
 // RejectionSample implements the rejection sampling logic for converting a
 // SHA512/256 hash to a value between 0-q
 func RejectionSample(q *big.Int, eHash *big.Int) *big.Int { // e' = eHash
-	e := eHash.Mod(eHash, q)
+	qGTZero := zero.Cmp(q) == -1
+	// e = the first |q| bits of e'
+	qBits := q.BitLen()
+	e := firstBitsOf(qBits, eHash)
+	// while e is not between 0-q
+	for !(qGTZero && e.Cmp(q) == -1) {
+		eHash := SHA512_256iOne(eHash)
+		e = firstBitsOf(qBits, eHash)
+	}
+	return e
+}
+
+func firstBitsOf(bits int, v *big.Int) *big.Int {
+	e := big.NewInt(0)
+	for i := 0; i < bits; i++ {
+		bit := v.Bit(i)
+		if 0 < bit {
+			e.SetBit(e, i, bit)
+		}
+	}
 	return e
 }
